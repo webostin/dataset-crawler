1bcf6c13a36d5afb84c9feea9a72de7e60669357
Michał Otfinowski <otfinowski@sigmeo.pl>
2019-02-20 10:43:58 +0100

    DecorateFormatter accept array as values and decorate many

--- Formatter/DecorateFormatter.php
+++ Formatter/DecorateFormatter.php
@@ -10,12 +10,38 @@ class DecorateFormatter implements FormatterInterface
 {
     public function format($value, $options = [])
     {
+        $prepend = '';
+        $append = '';
+
         if (isset($options['decorate_before'])) {
-            $value = $options['decorate_before'] . $value;
+            $prepend = $options['decorate_before'];
         }
         if (isset($options['decorate_after'])) {
-            $value .= $value . $options['decorate_after'];
+            $append = $options['decorate_after'];
         }
+
+        if (is_array($value)) {
+            $value = $this->decorateMany($value, $prepend, $append);
+        } else {
+            $value = $this->decorate($value, $prepend, $append);
+        }
+
+        return $value;
+    }
+
+    protected function decorateMany($values, $prepend = '', $append = '')
+    {
+        foreach ($values as &$value) {
+            $value = $this->decorate($value, $prepend, $append);
+        }
+
+        return $values;
+    }
+
+    protected function decorate($value, $prepend = '', $append = '')
+    {
+        $value = $prepend . $value;
+        $value .= $append;
 
         return $value;
     }

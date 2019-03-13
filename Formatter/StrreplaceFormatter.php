0f9cbfce793c51a50704749988b33a1338409ccc
Michał Otfinowski <otfinowski@sigmeo.pl>
2019-03-13 11:05:28 +0100

    StrreplaceFormatter trim option

--- Formatter/StrreplaceFormatter.php
+++ Formatter/StrreplaceFormatter.php
@@ -6,14 +6,20 @@ namespace Webostin\Component\DatasetCrawler\Formatter;
 
 use Webostin\Component\DatasetCrawler\FormatterInterface;
 
-class StrreplaceFormatter implements FormatterInterface
+class StrreplaceFormatter extends TrimFormatter implements FormatterInterface
 {
     public function format($value, $options = [])
     {
         $search = $options['str_search'];
         $replace = $options['str_replace'];
 
-        return str_replace($search, $replace, $value);
+        $value = str_replace($search, $replace, $value);
+
+        if (isset($options['trim'])) {
+            return parent::format($value, $options);
+        }
+
+        return $value;
     }
 
 }
